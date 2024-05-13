3a16942a620a94e14bc16ce6713720a09632962d
use-hunks <shunshu618@gmail.com>
2024-05-13 17:55:53 +0900

    Accept user input and display API results on transitioned screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
-import 'dart:io';
 import 'next_page.dart';
 
 void main() {
@@ -9,8 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,65 +15,26 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Talk with CHUPPY'),
+      home: const MyHomePage(title: 'Talk with CHAPPIE'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const tokenKey = 'OPENAI_API_KEY';
-  late final String token;
-  late final OpenAI openAI;
-  _MyHomePageState() {
-    // コンストラクター内でtokenとopenAIを初期化
-    token = Platform.environment[tokenKey] ?? '';
-    openAI = OpenAI.instance.build(
-      token: token,
-      baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 20)),
-    );
-  }
-  String content = 'What should I do for developing my app using flutter?';
-  
-  late Future<String> responseText;
-  @override
-  void initState(){
-    super.initState();
-    responseText = Future(
-      ()async{
-        final response = await openAI.onChatCompletion(
-          request: ChatCompleteText(
-            model: GptTurboChatModel(),
-            messages: [ 
-              Messages(
-                role: Role.system,
-                content : content,
-              ).toJson(),
-            ],
-            maxToken: 100,
-          ),
-      );
-      return response?.choices.last.message?.content ?? ''; 
-      }
-    );
-  }
-
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-
+  late String userInput;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,40 +45,28 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FutureBuilder<String>(
-              future: responseText,
-              builder: (context, snapshot){
-                if(snapshot.hasData){
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.blue),
-                    ),
-                    child:Text(snapshot.data ?? ''),
-                  );
-                }
-                if(snapshot.connectionState == ConnectionState.waiting){
-                  return CircularProgressIndicator();
-                }else if(snapshot.hasError){
-                  return Text('Error: ${snapshot.error}');
-                }else{
-                  return Text(
-                    snapshot.data ?? '',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16),
-                  );
-                }
-              }
+            Container(
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Enter a sentence'
+                ),
+                onChanged: (text){
+                  userInput = text;
+                },
+              )
             ),
             ElevatedButton(
+              child: Text('START'),
               onPressed: (){
                  // ここにボタンを押した時に呼ばれるコードを書く
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => NextPage()),
-            );
+                  MaterialPageRoute(builder: (context) => NextPage(userInput)),
+                );
               }, 
-              child: Text('START')),
+            ),
             const SizedBox(height: 20),
             Text(
               'You have pushed the button this many times:'
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,6 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'dart:io';
 
-class NextPage extends StatelessWidget{
+class NextPage extends StatefulWidget{
+  //initializer
+  NextPage(this.input);
+  String input;
+  @override
+  _NextPageState createState() => _NextPageState(this.input);
+}
+
+class _NextPageState extends State<NextPage>{
+  static const tokenKey = 'OPENAI_API_KEY';
+  late final String token;
+  late final OpenAI openAI;
+  late String content;
+  _NextPageState(String input) {
+    // コンストラクター内でtokenとopenAIを初期化
+    this.content = input;
+    token = Platform.environment[tokenKey] ?? '';
+    openAI = OpenAI.instance.build(
+      token: token,
+      baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 20)),
+    );
+  }
+  
+  late Future<String> responseText;
+  @override
+  void initState(){
+    super.initState();
+    responseText = Future(
+      ()async{
+        final response = await openAI.onChatCompletion(
+          request: ChatCompleteText(
+            model: GptTurboChatModel(),
+            messages: [ 
+              Messages(
+                role: Role.system,
+                content : content,
+              ).toJson(),
+            ],
+            maxToken: 100,
+          ),
+      );
+      return response?.choices.last.message?.content ?? ''; 
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +56,33 @@ class NextPage extends StatelessWidget{
       ),
       body: Center(
         child: Column(
-          children: [
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FutureBuilder<String>(
+              future: responseText,
+              builder: (context, snapshot){
+                if(snapshot.hasData){
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue),
+                    ),
+                    child:Text(snapshot.data ?? ''),
+                  );
+                }
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return CircularProgressIndicator();
+                }else if(snapshot.hasError){
+                  return Text('Error: ${snapshot.error}');
+                }else{
+                  return Text(
+                    snapshot.data ?? '',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16),
+                  );
+                }
+              }
+            ),
             Container(
               child:Text('next page'),
             )
