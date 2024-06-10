20c40dfdaa78349859413ace6cb7cac8a7bfead6
use-hunks <shunshu618@gmail.com>
2024-06-10 11:55:12 +0900

    added start_page_view.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'next_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import './view/conversation_view.dart';
+import 'package:ai_conversation/view/start_page_view.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: "./.env");
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,52 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const ConversationView(),
-      // const MyHomePage(title: 'Talk with CHAPPIE'),
+      home: const StartPageView(title: "Talk with CHAPPIE"),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              Text(
-                'Welcome to AI Conversation',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                child: Text('START'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NextPage()),
-                  );
-                },
-              ),
-            ])));
   }
 }
---
+++ lib/view/start_page_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:ai_conversation/next_page.dart';
+
+class StartPageView extends StatelessWidget{
+  const StartPageView({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to AI Conversation',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: const Text('START'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage()),
+                );
+              },
+            ),
+          ]
+        )
+      )
+    );
+  }
+}
